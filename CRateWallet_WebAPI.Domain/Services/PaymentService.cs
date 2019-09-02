26f90810beb454ba91b60c3bde0f92be35004034
FreePM <622707130011@dpu.ac.th>
2019-09-02 15:15:51 +0700

    Update

--- CRateWallet_WebAPI.Domain/Services/PaymentService.cs
+++ CRateWallet_WebAPI.Domain/Services/PaymentService.cs
@@ -1,11 +1,23 @@
 ﻿using CRateWallet_WebAPI.Domain.Interfaces;
+using CRateWallet_WebAPI.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CRateWallet_WebAPI.Domain.Services
 {
     public class PaymentService : IPaymentService
     {
+        private readonly IBaseService _baseService;
+        public PaymentService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public Task<ReturnDto<string>> GenerateQrCode(string username)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
